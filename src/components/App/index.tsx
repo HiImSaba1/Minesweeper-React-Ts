e35390d5134c4b587f10f8459a3e6b4f02aee4f0
Pavlos Saba <pavlossaba@gmail.com>
2020-03-13 18:12:52 +0200

    Finished grabAll Cells

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,102 +1,117 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import "./App.scss";
-import {generateCells} from "../utils";
+import { generateCells, openMultipleCells } from "../utils";
 import NumberDisplay from '../NumberDisplay';
 import Button from '../Button';
-import {Face, Cell, CellState} from '../../types';
+import { Face, Cell, CellState, CellValue } from '../../types';
 
-const App: React.FC = () =>{
-    const [cells, setCells] = useState<Cell[][]>( generateCells() );
-    const [face, setFace] = useState<Face>( Face.smile );
-    const [time, setTime] = useState<number>( 0 );
-    const [bombCounter, setbombCounter] = useState<number>( 10 );
-    const [live, setLive] = useState<boolean>( false );
+const App: React.FC = () => {
+    const [cells, setCells] = useState<Cell[][]>(generateCells());
+    const [face, setFace] = useState<Face>(Face.smile);
+    const [time, setTime] = useState<number>(0);
+    const [bombCounter, setbombCounter] = useState<number>(10);
+    const [live, setLive] = useState<boolean>(false);
     useEffect(() => {
-        const handleMouseDown = (): void  => {
+        const handleMouseDown = (): void => {
             setFace(Face.oh);
         };
         const handleMouseUp = (): void => {
             setFace(Face.smile);
         };
 
-        window.addEventListener ("mousedown", handleMouseDown );
-        window.addEventListener ("mouseup", handleMouseUp );
+        window.addEventListener("mousedown", handleMouseDown);
+        window.addEventListener("mouseup", handleMouseUp);
 
         return () => {
-            window.removeEventListener ("mousedown", handleMouseDown );
-            window.removeEventListener ("mouseup", handleMouseUp );
+            window.removeEventListener("mousedown", handleMouseDown);
+            window.removeEventListener("mouseup", handleMouseUp);
         };
-    },[]);
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         if (live && time < 999) {
             const timer = setInterval(() => {
                 setTime(time + 1);
-            },1000);
+            }, 1000);
             return () => {
                 clearInterval(timer);
             };
         }
-    },[live, time]);
+    }, [live, time]);
     const handleCellClick = (rowParam: number, colParam: number) => (): void => {
-       //Game start
-       if( !live ) {
-           setLive(true);
-    }
+        //Game start
+        if (!live) {
+            setLive(true);
+        }
+        const currentCell = cells[rowParam][colParam];
+        let newCells = cells.slice();
+        if ([CellState.flagged, CellState.visible].includes(currentCell.state)) {
+            return;
+        }
+        if (currentCell.value === CellValue.bomb) {
+
+        } else if (currentCell.value === CellValue.none) {
+            newCells = openMultipleCells(newCells, rowParam, colParam);
+            setCells(newCells);
+        } else {
+            newCells[rowParam][colParam].state = CellState.visible;
+            setCells(newCells);
+        }
+
     };
     const handleCellContext = (rowParam: number, colParam: number) => (
-        e: React.MouseEvent<HTMLDivElement,MouseEvent>
+        e: React.MouseEvent<HTMLDivElement, MouseEvent>
     ): void => {
-           e.preventDefault();
-           if(!live){
+        e.preventDefault();
+        if (!live) {
             return;
-           }
-           const currentCells = cells.slice();
-           const currentCell = cells[rowParam][colParam];
-           if(currentCell.state === CellState.visible){
+        }
+        const currentCells = cells.slice();
+        const currentCell = cells[rowParam][colParam];
+        if (currentCell.state === CellState.visible) {
             return;
-           }else if(currentCell.state === CellState.open){
+        } else if (currentCell.state === CellState.open) {
             currentCells[rowParam][colParam].state = CellState.flagged;
             setCells(currentCells);
             setbombCounter(bombCounter - 1);
-           }else if(currentCell.state === CellState.flagged){
+        } else if (currentCell.state === CellState.flagged) {
             currentCells[rowParam][colParam].state = CellState.open;
             setCells(currentCells);
             setbombCounter(bombCounter + 1);
         }
-     };
+    };
 
     const handleFaceClick = (): void => {
-        if(live){
-            setLive (false);
-            setTime (0);
+        if (live) {
+            setLive(false);
+            setTime(0);
             setCells(generateCells());
         }
     };
-   
-    const renderCells = (): React.ReactNode =>{
-        return cells.map((row,rowIndex) => row.map((cell, colIndex) => 
-        <Button key={`${rowIndex}-${colIndex}`} 
-        state={cell.state} 
-        value={cell.value} 
-        row={rowIndex} 
-        col={colIndex}
-        onClick = {handleCellClick}
-        onContext = {handleCellContext}
-        /> ))
+
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) => row.map((cell, colIndex) =>
+            <Button key={`${rowIndex}-${colIndex}`}
+                state={cell.state}
+                value={cell.value}
+                row={rowIndex}
+                col={colIndex}
+                onClick={handleCellClick}
+                onContext={handleCellContext}
+            />))
     }
-    return(
+    return (
         <div className="App">
-           <div className="Header">
-               <NumberDisplay value={bombCounter} />
-               <div className="Face" onClick={handleFaceClick} > 
-               <span role="img" aria-label="face" >
-                   {face}
-               </span>
-               </div>
-               <NumberDisplay value={time} />
-           </div>
-    <div className="Body">{renderCells()}</div>
+            <div className="Header">
+                <NumberDisplay value={bombCounter} />
+                <div className="Face" onClick={handleFaceClick} >
+                    <span role="img" aria-label="face" >
+                        {face}
+                    </span>
+                </div>
+                <NumberDisplay value={time} />
+            </div>
+            <div className="Body">{renderCells()}</div>
         </div>
     )
 };
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,35 +2,35 @@ import React from 'react';
 import "./Button.scss";
 import { CellState, CellValue } from '../../types';
 
-interface ButtonProps{
+interface ButtonProps {
     row: number;
     col: number;
     state: CellState;
     value: CellValue;
-    onClick(rowParam: number , colParam: number): (...args: any[]) => void;
-    onContext(rowParam: number , colParam: number): (...args: any[]) => void;
+    onClick(rowParam: number, colParam: number): (...args: any[]) => void;
+    onContext(rowParam: number, colParam: number): (...args: any[]) => void;
 }
-const Button: React.FC<ButtonProps> = ({row , col , onContext ,onClick, state , value}) => {
+const Button: React.FC<ButtonProps> = ({ row, col, onContext, onClick, state, value }) => {
     const renderContent = (): React.ReactNode => {
-        if(state === CellState.visible){
-            if(value === CellValue.bomb){
+        if (state === CellState.visible) {
+            if (value === CellValue.bomb) {
                 return (<span role="img" aria-label="bomb" >💣</span>);
-            }else if (value === CellValue.none){
+            } else if (value === CellValue.none) {
                 return null;
             }
             return value;
-        }else if (state === CellState.flagged){
+        } else if (state === CellState.flagged) {
             return (<span role="img" aria-label="flag" >⛳</span>);
         }
         return null;
     };
 
-    return <div className={`Button ${state === CellState.visible ? "visible" : "" } 
-    value-${value}`}  
-    onClick={onClick(row, col)} 
-    onContextMenu={onContext(row, col)} 
+    return <div className={`Button ${state === CellState.visible ? "visible" : ""} 
+    value-${value}`}
+        onClick={onClick(row, col)}
+        onContextMenu={onContext(row, col)}
     >
-            {renderContent()}
-        </div>
+        {renderContent()}
+    </div>
 };
 export default Button;
